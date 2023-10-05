adb732ff804df29639306747ae72a04eb0742df9
mark.nago@3ss.tv <nago.mark@student.ms.sapientia.ro>
2023-10-05 13:26:08 +0300

    [LAB2] - Done The Lab Exercises!

--- kotlin-exercises/labor-02/src/main/Data/Date.kt
+++ kotlin-exercises/labor-02/src/main/Data/Date.kt
@@ -1,12 +1,27 @@
 package main.Data
-
 import java.time.LocalDate
-import java.util.*
 
-data class Date(
+data class Date (
+
+
     val year: Int = LocalDate.now().year,
     val month: Int = LocalDate.now().monthValue,
     val day: Int = LocalDate.now().dayOfMonth
-)
+
+) : Comparable<Date>, Comparator<Date> {
+    override fun compareTo(other: Date): Int {
+        return this.year.compareTo(other.year)
+    }
+
+    override fun compare(o1: Date?, o2: Date?): Int {
+
+        if(o1 is Date && o2 is Date){
+            return o1.month.compareTo(o2.month)
+        }
+        return 0;
+    }
+}
 
 fun Date.isLeapYear() = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+fun Date.isValidDate() = day in 1..31 && month in 1..12 && year > 0
+
--- kotlin-exercises/labor-02/src/main/Data/Main.kt
+++ kotlin-exercises/labor-02/src/main/Data/Main.kt
@@ -1,13 +1,48 @@
 import main.Data.Date
-import main.Data.isLeapYear
+import main.Data.isValidDate
 
 fun main(args: Array<String>){
 
-    val today = Date()
-    println(today)
-    if(today.isLeapYear()){
-        println("$today is leap year!");
-    } else {
-        println("$today is not a leap year!");
+    val validDates = mutableListOf<Date>()
+    println("--- Invalid Dates ---")
+    while(validDates.count() < 10) {
+
+        val year = (-10..2030).random()
+        val month = (1..15).random()
+        val day = (0..40).random()
+
+        val date = Date(year,month,day)
+
+        if(date.isValidDate()) {
+            validDates.add(date)
+        } else {
+            println("The $date is invalid!")
+        }
+
+    }
+
+    println(" --- Valid Dates --- ")
+    validDates.forEach {
+        println(it)
+    }
+
+    // sorting the dates
+    validDates.sort()
+
+    println(" --- Sorted Valid Dates ---")
+    validDates.forEach {
+        println(it)
+    }
+
+    validDates.reverse()
+    println(" --- Reversed Valid Dates ---")
+    validDates.forEach {
+        println(it)
+    }
+
+    validDates.sortWith { o1, o2 -> o1.month.compareTo(o2.month) }
+    println(" --- Sorted Valid Dates By Month ---")
+    validDates.forEach {
+        println(it)
     }
 }
